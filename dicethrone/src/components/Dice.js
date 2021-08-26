0b215a494b6ab79b2610fead2e9aecfd07cec38b
dbrandt1990 <dvbrandt90@gmail.com>
2021-08-26 12:14:28 -0600

    added images to dice

--- dicethrone/src/components/Dice.js
+++ dicethrone/src/components/Dice.js
@@ -5,13 +5,31 @@ export const Dice = (props) => {
     let bolt = "fas fa-bolt"
     let skull = "fas fa-skull-crossbones"
     let heart = "fas fa-heart"
+
+    const setSymbol = (result) => {
+        if (result < 4 && result > 0) { return bolt }
+        else if (result > 3 && result < 6) { return heart }
+        else if (result === 6) { return skull }
+        else { return '' }
+    }
     return (
         <div id='dice'>
-            <div onClick={props.handleClick} className={`${player}`} id='die1'>{props.results[0]}</div>
-            <div onClick={props.handleClick} className={`${player}`} id='die2'>{props.results[1]}</div>
-            <div onClick={props.handleClick} className={`${player}`} id='die3'>{props.results[2]}</div>
-            <div onClick={props.handleClick} className={`${player}`} id='die4'>{props.results[3]}</div>
-            <div onClick={props.handleClick} className={`${player}`} id='die5'>{props.results[4]}</div>
+            <div onClick={props.handleClick} className={`${player}`} id='die1'>{props.results[0]}
+                <span className={setSymbol(props.results[0])}></span>
+            </div>
+            <div onClick={props.handleClick} className={`${player}`} id='die2'>{props.results[1]}
+                <span className={setSymbol(props.results[1])}></span>
+            </div>
+            <div onClick={props.handleClick} className={`${player}`} id='die3'>{props.results[2]}
+                <span className={setSymbol(props.results[2])}></span>
+            </div>
+            <div onClick={props.handleClick} className={`${player}`} id='die4'>{props.results[3]}
+                <span className={setSymbol(props.results[3])}></span>
+            </div>
+            <div onClick={props.handleClick} className={`${player}`} id='die5'>{props.results[4]}
+                <span className={setSymbol(props.results[4])}></span>
+            </div>
         </div>
     )
 }
+
